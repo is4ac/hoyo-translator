b296818c3a2a41a5d2a77f9b2cf0b61bdbd5e85a
is4ac <isaacsung@gmail.com>
2024-03-09 13:45:52 -0600

    Prettier change to 2 space tabs

--- src/commands/message/translate-hoyo-names.ts
+++ src/commands/message/translate-hoyo-names.ts
@@ -9,17 +9,17 @@ import { translateNames } from '../../utils/translate-utils.js';
 import { Command, CommandDeferType } from '../index.js';
 
 export class TranslateHoyoNames implements Command {
-    public names = [Lang.getRef('messageCommands.translateHoyoNames', Language.Default)];
-    public cooldown = new RateLimiter(1, 5000);
-    public deferType = CommandDeferType.PUBLIC;
-    public requireClientPerms: PermissionsString[] = [];
+  public names = [Lang.getRef('messageCommands.translateHoyoNames', Language.Default)];
+  public cooldown = new RateLimiter(1, 5000);
+  public deferType = CommandDeferType.PUBLIC;
+  public requireClientPerms: PermissionsString[] = [];
 
-    public async execute(
-        intr: MessageContextMenuCommandInteraction,
-        _data: EventData
-    ): Promise<void> {
-        const message = intr.targetMessage;
+  public async execute(
+    intr: MessageContextMenuCommandInteraction,
+    _data: EventData
+  ): Promise<void> {
+    const message = intr.targetMessage;
 
-        await InteractionUtils.send(intr, translateNames(message.content));
-    }
+    await InteractionUtils.send(intr, translateNames(message.content));
+  }
 }
--- src/utils/translate-utils.ts
+++ src/utils/translate-utils.ts
@@ -1,24 +1,12 @@
-const data = {
-    // Genshin Impact Characters
-    Traveler: '旅人',
-    旅人: 'Traveler',
-
-    Albedo: 'アルベド',
-    アルベド: 'Albedo',
-
-    Xingqiu: '行秋',
-    行秋: 'Xingqiu',
-};
-
 export function translateNames(message: string): string {
-    let translatedNames: string[] = [];
+  let translatedNames: string[] = [];
 
-    Object.keys(data).forEach(name => {
-        const regex = new RegExp(name, 'i');
-        if (regex.test(message)) {
-            translatedNames.push(name);
-        }
-    });
+  Object.keys(data).forEach(name => {
+    const regex = new RegExp(name, 'i');
+    if (regex.test(message)) {
+      translatedNames.push(name);
+    }
+  });
 
-    return translatedNames.map(name => `${name} = ${data[name]}`).join('\n');
+  return translatedNames.map(name => `${name} = ${data[name]}`).join('\n');
 }
